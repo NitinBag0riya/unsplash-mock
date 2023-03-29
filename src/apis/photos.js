2f13482201e1d1c409838b88b626bb02cde51ee6
Nitin Bagoriya <nitin-bagoriya@lidolearning.com>
2023-03-29 09:30:30 +0530

    playing with code

--- src/apis/photos.js
+++ src/apis/photos.js
@@ -1,7 +1,28 @@
 import { UNSPLASH_ENDPOINT } from 'constants';
 
-function getPictures({page = 0, perPageCount = 20 }){
-    return fetch(`${UNSPLASH_ENDPOINT}&per_page=${perPageCount}&page=${page}`).then(response => response).then(data => data.json());
+// REACT.18 multiple times lifecycle update |  creating wrapper for handling API 
+// OR 
+// use third party packages.
+
+
+
+const createFetch = () => {
+    const fetchMap = {};
+      return (url, options) => {
+        if (!fetchMap[url]) {
+          fetchMap[url] = fetch(url, options).then((res) => res.json());
+        }
+      return fetchMap[url];
+    };
 };
+
+// end here //
+
+
+function getPictures({page = 0, perPageCount = 20 }){
+    let pictures = fetch(`${UNSPLASH_ENDPOINT}&per_page=${perPageCount}&page=${page}`).then(response => response).then(data => data.json());
+    return createFetch(pictures);
+};
+
 
 export default getPictures;
